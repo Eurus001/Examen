55307aa8e912e1eef19d166acdd4035a239a5968
Javier <villayali2001@gmail.com>
2023-05-03 14:56:09 -0500

    Segundo comit, declaracion de funcion

--- Examen.c
+++ Examen.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+void ImprimirFibonacci(int n){
+
+}
+
 int main(){
 
-    
+    int n;
+
+    printf("Ingrese un numero entero positivo n para generar los primeros n digitos de la serie de fibonacci");
+
+    scanf("%d", &n);
+
+    if (n > 0){
+
+
+
+    } else {
+        printf("Su numero no es un entero positivo");
+    }
 
 
     return 0; 
