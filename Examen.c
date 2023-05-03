1cf081a56bac233dc0e2e902c9d0ca1dfcd7cbdc
Javier <villayali2001@gmail.com>
2023-05-03 15:21:59 -0500

    Terminado el programa

--- Examen.c
+++ Examen.c
@@ -2,19 +2,33 @@
 
 void ImprimirFibonacci(int n){
 
+    int a, b, c;
+
+    a = b = 1;
+   
+    printf("El termino 1 de la serie de fibonacci es %d \n El termino 2 de la serie de fibonacci es %d ", a, b);
+
+    for(int i = 1; i <= n-2; i++) {
+
+        c = a + b;
+        printf("El termino %d de la serie de fibonacci es: %d \n", (i + 2) , c);
+        a = b;
+        b = c;
+    }
+    
 }
 
 int main(){
 
     int n;
 
-    printf("Ingrese un numero entero positivo n para generar los primeros n digitos de la serie de fibonacci");
+    printf("Ingrese un numero entero positivo n para generar los primeros n digitos de la serie de fibonacci: \n");
 
     scanf("%d", &n);
 
     if (n > 0){
 
-
+        ImprimirFibonacci(n);
 
     } else {
         printf("Su numero no es un entero positivo");
